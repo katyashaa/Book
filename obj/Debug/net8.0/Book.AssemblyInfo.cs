64478d22ef9f711547a1b483e8375695ac73a8d0
shadr <shadrkatya@mail.ru>
2025-01-16 10:04:31 +0300

    --

--- obj/Debug/net8.0/Book.AssemblyInfo.cs
+++ obj/Debug/net8.0/Book.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Book")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+206476bbf20816ff166f6cd07f9a9cf45f87fed0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+33d18bafd57aba2a65ea1bf25ca75daf5da81595")]
 [assembly: System.Reflection.AssemblyProductAttribute("Book")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Book")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
